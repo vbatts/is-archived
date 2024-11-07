3bfae32218a089af9916b078b999ede48b3b3f89
Vincent Batts <vbatts@hashbangbash.com>
2024-11-06 21:18:41 -0500

    pkg/cratesio: add support for Cargo.lock files
    Fixes #2
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- pkg/cratesio/check.go
+++ pkg/cratesio/check.go
@@ -9,7 +9,7 @@ import (
 	"github.com/vbatts/is-archived/pkg/check"
 )
 
-func ToCheck(c *Cargo) ([]check.Check, error) {
+func ToCheckCargo(c *Cargo) ([]check.Check, error) {
 	toCheck := []check.Check{}
 
 	if c.Package.Repository != "" {
@@ -30,7 +30,7 @@ func ToCheck(c *Cargo) ([]check.Check, error) {
 		if err != nil {
 			return toCheck, fmt.Errorf("failed fetching %q: %w", dep, err)
 		}
-		if strings.ToLower(s.Crate.ID) != strings.ToLower(dep) {
+		if !strings.EqualFold(s.Crate.ID, dep) {
 			logrus.Infof("%q does not match %q", s.Crate.ID, dep)
 			continue
 		}
@@ -46,6 +46,46 @@ func ToCheck(c *Cargo) ([]check.Check, error) {
 		toCheck = append(toCheck, check.Check{
 			Lang:    Name,
 			PkgName: dep,
+			VcsUrl:  u,
+		})
+	}
+
+	return toCheck, nil
+}
+
+func ToCheckCargoLock(cl *CargoLock) ([]check.Check, error) {
+	toCheck := []check.Check{}
+
+	for _, pkg := range cl.Package {
+		s, err := FetchSingle(pkg.Name)
+		if err != nil {
+			return toCheck, fmt.Errorf("failed fetching %q: %w", pkg, err)
+		}
+		if !strings.EqualFold(s.Crate.ID, pkg.Name) {
+			logrus.Infof("%q does not match %q", s.Crate.ID, pkg.Name)
+			continue
+		}
+		if s.Crate.Repository == "" {
+			logrus.Infof("%q does not list a repository", s.Crate.ID)
+			continue
+		}
+		/* was beginning on this path, but they can have a number of schema/protocols, that are not https/http
+		if pkg.Source != "" && !pkg.IsSourceRegistry() {
+			u, err := urlpkg.Parse(pkg.Source)
+		}
+		*/
+		u, err := urlpkg.Parse(s.Crate.Repository)
+		if err != nil {
+			logrus.Infof("%q did not parse correctly", s.Crate.Repository)
+			continue
+		}
+		if strings.HasSuffix(u.Path, ".git") {
+			i := strings.LastIndex(u.Path, ".git")
+			u.Path = u.Path[:i]
+		}
+		toCheck = append(toCheck, check.Check{
+			Lang:    Name,
+			PkgName: pkg.Name,
 			VcsUrl:  u,
 		})
 	}
