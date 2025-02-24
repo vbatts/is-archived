vbatts/is-archived

commit c881d7c65dc5f5f01005d461d9ff6190dadb05c0

*.go: add a version, and include as http UserAgent
If folks use this tool, give a hint to the different servcies about the
tool that is causing the traffic patterns.

Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

pkg/npm/api.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/sirupsen/logrus"
+	"github.com/vbatts/is-archived/version"
 )
 
 // https://www.edoardoscibona.com/exploring-the-npm-registry-api
@@ -31,7 +32,15 @@ func isEndpointAvailable() bool {
 }
 
 func FetchSingle(pkg string) (*Package, error) {
-	resp, err := http.Get(fmt.Sprintf("%s/%s", apiEndpoint, pkg))
+	u := fmt.Sprintf("%s/crates/%s", apiEndpoint, pkg)
+
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, fmt.Errorf("fetching %q: %w", u, err)
+	}
+	req.Header.Set("User-Agent", fmt.Sprintf("%s/%s", version.Project, version.Version))
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}

pkg/cratesio/api.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/vbatts/is-archived/version"
 )
 
 /*
@@ -59,7 +61,14 @@ func loadSingle(rdr io.Reader) (*Single, error) {
 // FetchSingle makes a call to the crates.io API regarding pkgname, and populates a returned Single.
 func FetchSingle(pkgname string) (*Single, error) {
 	u := fmt.Sprintf("%s/crates/%s", apiEndpoint, pkgname)
-	resp, err := http.Get(u)
+
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, fmt.Errorf("fetching %q: %w", u, err)
+	}
+	req.Header.Set("User-Agent", fmt.Sprintf("%s/%s", version.Project, version.Version))
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("fetching %q: %w", u, err)
 	}

main.go
@@ -14,12 +14,14 @@ import (
 	_ "github.com/vbatts/is-archived/pkg/golang"
 	_ "github.com/vbatts/is-archived/pkg/npm"
 	"github.com/vbatts/is-archived/pkg/types"
+	"github.com/vbatts/is-archived/version"
 )
 
 func main() {
 	app := cli.App{
-		Name:  "is-archived",
-		Usage: "check your project's dep's upstreams for being archived projects",
+		Name:    "is-archived",
+		Usage:   "check your project's dep's upstreams for being archived projects",
+		Version: version.Version,
 		Before: func(c *cli.Context) error {
 			logrus.SetOutput(os.Stderr)
 			if os.Getenv("DEBUG") != "" {

pkg/gh/archived.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/google/go-github/github"
+	"github.com/vbatts/is-archived/version"
 	"golang.org/x/oauth2"
 )
 
@@ -32,6 +33,7 @@ func New(ctx context.Context, token string) Handler {
 	} else {
 		h.Client = github.NewClient(nil)
 	}
+	h.Client.UserAgent = fmt.Sprintf("%s/%s", version.Project, version.Version)
 	return h
 }
 

version/version.go
@@ -1,0 +1,6 @@
+package version
+
+const (
+	Project = "github.com/vbatts/is-archived"
+	Version = "0.1"
+)

commit b7f84bfc2abfb7edf4326cd8045e5dbabab5c34e

pkg/npm: fix API path
regression from c881d7c

Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

pkg/npm/api.go
@@ -32,7 +32,7 @@ func isEndpointAvailable() bool {
 }
 
 func FetchSingle(pkg string) (*Package, error) {
-	u := fmt.Sprintf("%s/crates/%s", apiEndpoint, pkg)
+	u := fmt.Sprintf("%s/%s", apiEndpoint, pkg)
 
 	req, err := http.NewRequest("GET", u, nil)
 	if err != nil {